4c75a5eccc4753532a260a3a6912d94762120030
Benoît Latinier <benoit@latinier.fr>
2018-03-14 21:21:42 +0100

    fix scheduling ghost fix script

--- scripts/fix_schedule.sh
+++ scripts/fix_schedule.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sqlite3 /var/www/voyage/blog/content/data/ghost.db "update posts set status='published' where published_at < datetime('now') and status='scheduled'"
+sqlite3 /var/www/voyage/blog/content/data/ghost.db "update posts set status='published' where published_at < datetime('now', 'localtime') and status='scheduled'"
